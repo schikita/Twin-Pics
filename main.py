3c956a68726139c922460de296e09dd9ce79ce60
Zortea Roman <romeozortea@gmail.com>
2025-04-16 21:51:46 +0200

    Image

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
+import cv2
+import os
 from core.game import Game
-
+image_path = os.path.join('C:\\Users\\romeo\\Documents\\GitHub\\Twin-Pics\\assets\\images\\player.png')
+image = cv2.imread(image_path)
+if image is None:
+    print("Error")
+else:
+    cv2.imshow("Success", image)
 if __name__ == "__main__":
     pygame.init()
     Game().run()
