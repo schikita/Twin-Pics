2b7ed05b931c5f3efca2f2d5e1959d86b5841537
Serhei <s.chikita@mail.ru>
2025-04-19 19:01:49 +0300

    added music

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import pygame
 import cv2
 import os
 from core.game import Game
-image_path = os.path.join('C:\\Users\\romeo\\Documents\\GitHub\\Twin-Pics\\assets\\images\\player.png')
+image_path = os.path.join('player.png')
 image = cv2.imread(image_path)
 if image is None:
     print("Error")
 else:
-    pass
+    cv2.imshow("Success", image)
 if __name__ == "__main__":
-
+    pygame.mixer.pre_init(44100, -16, 2, 512)
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
-    pygame.display.set_caption("Twin Pics")
     Game().run()
     pygame.quit()
--- scenes/first_person.py
+++ scenes/first_person.py
@@ -4,21 +4,43 @@ import os
 import numpy as np
 from objects.player import Player
 from core.config import TEXT_COLOR
+
 image_path = os.path.join('assets//images//motel-room.png')
+music_path = os.path.join('assets', 'sound', 'motel-music.mp3')
+
 #Hello
 class FirstPersonScene:
     def __init__(self, screen):
         self.screen = screen
         self.font = pygame.font.SysFont("Courier", 22)
         self.input = ""
-        self.log = ["Ты очнулся в номере мотеля в Твин Пиксе..."]
+        self.log = ["Ты очнулся в номере мотеля в Твин Пиксе...\n"
+        "1. Осмотреться\n"
+        "2. Выглянуть в окно\n"
+        "3. Выйти на улицу\n"
+        "4. ..."]
         self.player = Player()
-        # картинка мотеля + музыка на старом радио
-        image = cv2.imread(image_path)
+        self.background = self._load_background(image_path)
+        self._setup_music()
+
+
+    def _setup_music(self):
+        if os.path.exists(music_path):
+            pygame.mixer.music.load(music_path)
+            pygame.mixer.music.set_volume(0.2)
+            pygame.mixer.music.play(-1)
+        else:
+            print(f'Музыка не найдена: {music_path}')
+
+    def _load_background(self, path):
+        image = cv2.imread(path)
+        if image is None:
+            raise FileExistsError(f'Фоновое изображение  не найдено: {image_path}')
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         surf = pygame.surfarray.make_surface(image)
         rotated = pygame.transform.rotate(surf, -90)
-        self.background = pygame.transform.scale(rotated, self.screen.get_size())
+        return pygame.transform.scale(rotated, self.screen.get_size())
+    
 
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
